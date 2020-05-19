207d74ad97e6030fd66ace75b4e1e8d29c23f138
Brandon Jenkins <brandon_jenkins0@icloud.com>
2020-05-19 09:00:48 -0600

    transfer

--- Hate-Memes/creating_image_dirs.py
+++ Hate-Memes/creating_image_dirs.py
@@ -5,7 +5,7 @@ pd.set_option('display.max_columns', 500)
 # Loading in the train, val, test labels
 train_labels = pd.read_json("Hate Memes/data/train.jsonl", lines=True)
 val_labels = pd.read_json("Hate Memes/data/dev.jsonl", lines=True)
-test_labels = pd.read_json("Hate Memes/data/test.jsonl", lines=True)
+test_labels = pd.read_json("Hate-Memes/data/test.jsonl", lines=True)
 
 # Writing text files to create the desired folder structure
 train_labels.loc[train_labels['label'] == 1, 'img'].to_csv("Hate Memes/data/train_hate.txt", sep=' ', index=False)
